7ad0213beb0c98865c85d2a689cb33b1c4786884
Muhammad Awais <kingyt6624@gmail.com>
2025-08-10 00:30:19 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      this is my blob app
+      <h1>Welcome to the MERN Blob App</h1>
+      <p>This application allows you to upload and manage blobs.</p>
+    </div>
+  );
+};
+
+export default App;
